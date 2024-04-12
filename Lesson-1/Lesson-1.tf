d0b2dbc7abccf8a0da4e5b8f5e14e185558e6eda
Ilya Kurbatov <ilya-balon@mail.ru>
2024-04-12 22:37:51 +1000

    Add annotations

--- Lesson-1/Lesson-1.tf
+++ Lesson-1/Lesson-1.tf
@@ -1,25 +1,30 @@
 resource "yandex_compute_instance" "myUbuntu" {
-  name        = "test2"
-  zone        = "ru-central1-a"
+  name               = "test2"
+  zone               = "ru-central1-a"
   maintenance_policy = "restart"
 
 
   resources {
-    cores  = 2
-    memory = 1
+    # Доля CPU в %
     core_fraction = 5
+    cores         = 2
+    memory        = 1
+
   }
   boot_disk {
     initialize_params {
+      # image_id Ubuntu https://yandex.cloud/ru/marketplace/products/yc/ubuntu-2204-lts-oslogin
       image_id = "fd8l45jhe4nvt0ih7h2e"
     }
   }
 
   network_interface {
+    # идентификатор подсети https://console.yandex.cloud/folders/b1g8kbqq92bvsk3h7i1u/vpc/network/enpeiguhevrqhejt3l7v/overview
     subnet_id = "e9berr3calqh9d4rpkcb"
   }
 
   scheduling_policy {
+    # Сделать ВМ прерываемой(дешевле)
     preemptible = "true"
   }
 
--- Lesson-1/default.tf
+++ Lesson-1/default.tf
@@ -1,19 +1,22 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "0.115.0"
     }
   }
 }
 
 locals {
+  # Идентификатор папки https://console.yandex.cloud/folders/b1g8kbqq92bvsk3h7i1u?section=service-accounts
   folder_id = "b1g8kbqq92bvsk3h7i1u"
+  # Идентификатор аккаунта https://console.yandex.cloud/folders/b1g8kbqq92bvsk3h7i1u/service-account/ajegt9po5pbfon86qc6e
   cloud_id = "ajegt9po5pbfon86qc6e"
 }
 
 provider "yandex" {
   //token                    = "auth_token_here"
+  # Путь до авторизованного ключа
   service_account_key_file = "/home/ilya/.secrets/authorized_key.json"
   cloud_id                 = local.cloud_id
   folder_id                = local.folder_id
